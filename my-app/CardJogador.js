e3ca8d3c21ce98623a5b9dd0f5b1bb4f966b7d11
Emanoel Silva <emanoelsilva.jua@gmail.com>
2023-10-19 11:21:55 -0300

    Buscar jogador

---
+++ my-app/CardJogador.js
@@ -0,0 +1,22 @@
+import { View } from "react-native-web"
+
+const CardJogador = () => {
+
+
+    return (
+        <View>
+
+        </View>
+
+    );
+    
+
+}
+
+export default CardJogador = () => {
+
+
+};
+
+
+
