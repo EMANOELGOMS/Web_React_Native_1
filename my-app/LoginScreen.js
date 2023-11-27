54f12aae3f2089e0d5d62985d5afa913e5836f1b
Emanoel Silva <emanoelsilva.jua@gmail.com>
2023-11-27 13:33:49 -0300

    Trazendo temperatura

--- my-app/LoginScreen.js
+++ my-app/LoginScreen.js
@@ -1,5 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, Button, StyleSheet } from 'react-native';
+
+const WeatherComponent = () => {
+  const [temperature, setTemperature] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://api.thingspeak.com/channels/2352930/fields/1.json?api_key=TMGQ76ZQHSANBUUX&results=1');
+        
+        if (response.ok) {
+          const data = await response.json();
+          const latestTemperature = data.feeds[0]?.field1;
+          setTemperature(latestTemperature);
+        } else {
+          console.error('Erro ao buscar os dados da API');
+        }
+      } catch (error) {
+        console.error('Erro na requisição:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <View style={styles.weatherContainer}>
+      {temperature !== null ? (
+        <Text style={styles.weatherText}>A temperatura atual é: {temperature} °C</Text>
+      ) : (
+        <Text style={styles.weatherText}>Carregando temperatura...</Text>
+      )}
+    </View>
+  );
+};
 
 const LoginScreen = ({ navigation }) => {
   const handleLogin = () => {
@@ -9,8 +43,9 @@ const LoginScreen = ({ navigation }) => {
 
   return (
     <View style={styles.container}>
-      <Text style = {styles.title}>Tela de Login</Text>
+      <Text style={styles.title}>Tela de Login</Text>
       <Button title="Login" onPress={handleLogin} />
+      <WeatherComponent />
     </View>
   );
 };
@@ -25,7 +60,16 @@ const styles = StyleSheet.create({
     fontSize: 24,
     fontWeight: 'bold',
     marginTop: 20,
-    marginBottom: 10,}
+    marginBottom: 10,
+  },
+  weatherContainer: {
+    marginTop: 20,
+    alignItems: 'center',
+  },
+  weatherText: {
+    fontSize: 16,
+    marginTop: 10,
+  },
 });
 
 export default LoginScreen;
